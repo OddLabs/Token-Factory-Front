baba46d366ebe01635292f2ea95b41e949646fc2
grossiwm <grossiwm@gmail.com>
2024-08-24 19:33:11 -0300

    add web3 lib and simple dapp page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Web3 from 'web3';
 
 function App() {
+  const [account, setAccount] = useState('');
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
+
+  const loadBlockchainData = async () => {
+    if (window.ethereum) {
+      const web3 = new Web3(window.ethereum);
+      try {
+        await window.ethereum.enable();
+        const accounts = await web3.eth.getAccounts();
+        setAccount(accounts[0]);
+      } catch (error) {
+        console.error('User denied account access');
+      }
+    } else if (window.web3) {
+      const web3 = new Web3(window.web3.currentProvider);
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    } else {
+      console.error('No Ethereum provider detected');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple React DApp</h1>
+      <p>Connected Account: {account}</p>
     </div>
   );
 }
